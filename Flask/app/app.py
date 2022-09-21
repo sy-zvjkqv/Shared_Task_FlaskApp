b25524c206996a431be90b51b54bcd8f309d1aa3
sy-zvjkqv <zvjkqv@gmail.com>
2022-09-21 17:14:56 +0900

    make readme and header on html

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -15,13 +15,16 @@ def post():
     select = request.form.get('radio')
     if select=="東京":
         code=estimater(text,select)
+        zoom=10
     else:
         select="全国"
         code=estimater(text,select)
+        zoom=6
     start_coords,sw,se,ne,nw=polygon(code,select)
     #folium_map=view(name,select)
     #folium_map.save('templates/map.html')
-    return render_template("map.html",text=text,code=code, start_coords= start_coords,sw=sw,se=se,ne=ne,nw=nw)
+    return render_template("map.html",text=text,code=code, start_coords= start_coords,sw=sw,se=se,ne=ne,nw=nw,zoom=zoom)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5125)
+    #app.run(debug=True,port=5001)
